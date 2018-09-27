60b05bbe9f80b620991e18868ee7e9eb101c2eb4
bangbang93 <bangbang93@163.com>
2018-09-27 18:31:02 +0800

    下载所有分辨率

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ import * as path from 'path'
   //wallpaper
   const page = await request.get('https://cloud.gmo.jp/anzu/')
   const $ = cheerio.load(page)
-  const links = $('.remodal li:nth-child(1) a')
+  let links = $('.remodal li a')
   let urls: string[] = links.map((i, link) => cheerio(link).attr('href')).toArray() as any[]
 
   let i = 0
   for (const url of urls) {
-    console.log(`${++i} / ${urls.length}`)
+    console.log(`wallpaper ${++i} / ${urls.length}`)
     const file = await request.get('https://cloud.gmo.jp/anzu/' + url, {encoding: null})
     await fs.outputFile(path.join('download', url.includes('SP')? 'phone': 'pc', path.basename(url)), file)
   }
@@ -24,7 +24,7 @@ import * as path from 'path'
 
   i = 0
   for (const url of urls) {
-    console.log(`${++i} / ${urls.length}`)
+    console.log(`emoji ${++i} / ${urls.length}`)
     const file = await request.get('https://cloud.gmo.jp/anzu/' + url, {encoding: null})
     await fs.outputFile(path.join('download/emotion', path.basename(url)), file)
   }
