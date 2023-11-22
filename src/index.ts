c5fc6f0da9f548f682e2e405eddabb08bfbeef86
bangbang93 <bangbang93@163.com>
2023-11-22 09:32:52 +0800

    style: setup biome

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,44 @@
-import * as fse from 'fs-extra'
-import cheerio from 'cheerio'
-import got from 'got'
-import path from 'path'
+import * as fse from "fs-extra"
+import cheerio from "cheerio"
+import got from "got"
+import path from "path"
 
+const BASE = "https://cloud.gmo.jp/anzu/"
 
 //wallpaper
-const page = await got.get('https://cloud.gmo.jp/anzu/')
+const page = await got.get(BASE)
 const $ = cheerio.load(page.body)
-let links = $('.remodal li a')
-let urls: string[] = links.map((i, link) => cheerio(link).attr('href')).toArray() as any[]
+let links = $(".remodal li a")
+let urls: string[] = links
+  .map((i, link) => cheerio(link).attr("href"))
+  .toArray() as any[]
 
 let i = 0
 for (const url of urls) {
   console.log(`wallpaper ${++i} / ${urls.length}`)
-  const filename = path.join('download', url.includes('SP')? 'phone': 'pc', path.basename(url))
+  const filename = path.join(
+    "download",
+    url.includes("SP") ? "phone" : "pc",
+    path.basename(url),
+  )
   if (await fse.pathExists(filename)) {
     continue
   }
-  const file = await got.get('https://cloud.gmo.jp/anzu/' + url, {responseType: 'buffer'})
+  const file = await got.get(`${BASE}${url}`, { responseType: "buffer" })
   await fse.outputFile(filename, file)
 }
 
 //em
-const ems = $('.stamp-wrap img')
-urls = ems.map((i, em) => cheerio(em).attr('src')).toArray() as any[]
+const ems = $(".stamp-wrap img")
+urls = ems.map((i, em) => cheerio(em).attr("src")).toArray() as any[]
 
 i = 0
 for (const url of urls) {
   console.log(`emoji ${++i} / ${urls.length}`)
-  const filename = path.join('download/emotion', path.basename(url))
+  const filename = path.join("download/emotion", path.basename(url))
   if (await fse.pathExists(filename)) {
     continue
   }
-  const file = await got.get('https://cloud.gmo.jp/anzu/' + url, {responseType: 'buffer'})
+  const file = await got.get(`${BASE}${url}`, { responseType: "buffer" })
   await fse.outputFile(filename, file)
 }
